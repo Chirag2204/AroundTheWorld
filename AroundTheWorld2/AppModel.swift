422421104d3f09dcdb49ef9c9cbda9b69862ba2c
Chirag <chandakchirag22@gmail.com>
2026-08-22 19:49:22 +0530

    Initial Commit

---
+++ AroundTheWorld2/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  AroundTheWorld2
+//
+//  Created by Chirag Chandak on 22/08/26.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ AroundTheWorld2/AroundTheWorld2App.swift
@@ -0,0 +1,34 @@
+//
+//  AroundTheWorld2App.swift
+//  AroundTheWorld2
+//
+//  Created by Chirag Chandak on 22/08/26.
+//
+
+import SwiftUI
+
+@main
+struct AroundTheWorld2App: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
